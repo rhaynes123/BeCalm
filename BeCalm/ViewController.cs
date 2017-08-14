fdd0e35c7c5b3bb1fa27c2286b31dec55232327a
Richard <rahayn@yahoo.com>
2017-08-13 22:27:12 -0400

    BeCalm
    This App is a test of using the Xamarin studio IDE to run on iOS
    devices. Its a short application for reminding a user to stay calm in
    stressful situations

---
+++ BeCalm/ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using UIKit;
+
+namespace BeCalm
+{
+    
+    public partial class ViewController : UIViewController
+    {
+        partial void UIButton9_TouchUpInside(UIButton sender)
+        {
+            throw new NotImplementedException();
+
+        }
+      
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
